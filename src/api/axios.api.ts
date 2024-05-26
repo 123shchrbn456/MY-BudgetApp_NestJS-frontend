9de2a61a7271f164ed832b8735356a010c4e0f7e
Igor <qwerty2244668800@gmail.com>
2024-05-26 16:02:56 +0300

    baseURL is changed

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { getTokenFromLocalStorage } from '../helpers/localstorage.helper'
 
 export const instance = axios.create({
-	baseURL: 'http://localhost:3001/api',
+	// baseURL: 'http://localhost:3001/api',
+	baseURL: 'https://my-budget-app-nest-js-backend.vercel.app/api',
 	headers: {
 		Authorization: `Bearer ` + getTokenFromLocalStorage() || '',
 	},
